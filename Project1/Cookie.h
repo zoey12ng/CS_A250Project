a97f1f02e66e8a0241ac56954e82b5f4a040c7b7
Mauricio Alvarez Leon <alvarez.mauricioTM@gmail.com>
2021-05-06 14:50:07 -0700

    cleaned up partA (cookie.h)

--- Project1/Cookie.h
+++ Project1/Cookie.h
@@ -11,10 +11,10 @@ class CookieType{
     public:
         CookieType(){ calories = 0;}
         CookieType(string name, size_t calorieNum){ cookieName = name; calories = calorieNum;}
-        string getName(){ return cookieName;}
-        size_t getCalories(){return calories;}
-        void setName(string newName){cookieName = newName;}
-        void setCalories(size_t newCalories){calories = newCalories;}
+        string getName() const{ return cookieName;}
+        size_t getCalories() const{return calories;}
+        void setName(const string newName){cookieName = newName;}
+        void setCalories(const size_t newCalories){calories = newCalories;}
         ~CookieType(){}
     protected:
         string cookieName;
@@ -26,21 +26,21 @@ class Cookie : public CookieType{
     public:
         Cookie(){numOfServings = 0;}
         Cookie(string name, size_t calorieNum, set<string> ingredients, size_t numOfServings){cookieName = name;};
-        size_t getServings(){return numOfServings;}
-        set<string> getIngredients(){return ingredients;}
+        size_t getServings() const{return numOfServings;}
+        set<string> getIngredients() const{return ingredients;}
         Cookie& getCookie(){return *this;}
         ~Cookie(){}
-        
-        void setServings(size_t newServings){numOfServings = newServings;}
-        void setIngredients(vector<string> newIngredients){
+
+        void setServings(const size_t newServings){numOfServings = newServings;}
+        void setIngredients(const vector<string> &newIngredients){
             //ingredients = newIngredients;
             }
-        void printRecipe(){
+        void printRecipe() const{
             cout << "Cookie: " << cookieName << endl;
             cout << "Number of Servings: " << numOfServings << endl;
         }
 
-        void printCalories(){
+        void printCalories() const{
             cout << "Cookie: " << cookieName << endl;
             cout << "Number of Calories: " << calories << endl;
         }
