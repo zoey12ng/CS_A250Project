c03ba6f0ab3f513baa7e1712eef8e7b769636064
Thu Nguyen <70258727+ThuNguyen12@users.noreply.github.com>
2021-05-13 00:10:04 -0700

    Update CookieType.h

--- Project1/CookieType.h
+++ Project1/CookieType.h
@@ -12,10 +12,13 @@
 #ifndef COOKIETYPE_H
 #define COOKIETYPE _H
 
+#include //???? not <iostream> there is no need to include iostream in this file, 
+//because you are not printing anything,(ONLY the ones needed in this file)
+
 class CookieType{
     public:
         CookieType();
-        CookieType(string name, size_t calorieNum);
+        CookieType(string name, size_t calorieNum); //how do you pass an object (string)? I try const string& name but it said error 
         
         void setName(const string newName);
         void setCalories(const size_t newCalories);
