60534e7249ad08b4b611f6e6b648255f1891d4bb
Thu Nguyen <70258727+ThuNguyen12@users.noreply.github.com>
2021-05-18 15:13:10 -0700

    Update CookieList.cpp
    Need to check addCookie, create new node to the list

--- Project1/CookieList.cpp
+++ Project1/CookieList.cpp
@@ -20,17 +20,24 @@ using namespace std;
 // Function addCookie
 void CookieList::addCookie(const Cookie& aCookie)
 {
-	last->setNext(new Node(aCookie, nullptr));
-	last = last->getNext();
+	if( count == 0)
+	{
+		//add a new node to the list
+	}
+	else
+	{
+		last->setNext(new Node(aCookie, nullptr));
+		last = last->getNext();
+	}
+	count++;
 }
 
 //addCookie overloaded
 void CookieList::addCookie(const std::string& newName, size_t newCalories,
 		const std::set<std::string>& newIngredients, size_t newServings)
 {
-	Cookie(newName, newCalories, newIngredients, newServings);
-	last->setNext(new Node(Cookie(newName, newCalories, newIngredients, newServings), nullptr));
-	last = last->getNext();
+	Cookie aCookie(newName, newCalories, newIngredients, newServings);
+	addCookie(aCookie);
 }
 
 // Accessor function
