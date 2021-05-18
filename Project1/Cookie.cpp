4f64240e02e478cc633ba953b40a732fb753b3f5
Thu Nguyen <70258727+ThuNguyen12@users.noreply.github.com>
2021-05-17 23:08:03 -0700

    Update Cookie.cpp

--- Project1/Cookie.cpp
+++ Project1/Cookie.cpp
@@ -14,45 +14,57 @@
 #include <iostream>
 #include <set>
 #include <vector>
+
 using namespace std;
 
 Cookie::Cookie(const string& name, size_t calorieNum, const set<string>& newIngredients, 
                size_t newServings) : CookieType(name, calorieNum)
-   {                                                                                      
-       ingredients = newIngredients;
-       numOfServings = newServings;
-    }
-          
-    
-
-void Cookie::setServings(const size_t newServings) {
-        numOfServings = newServings;
-    }
+{                                                                                      
+    ingredients = newIngredients;
+    numOfServings = newServings;
+}
+ 
+void Cookie::setServings(const size_t newServings) 
+{
+    numOfServings = newServings;
+}
         
-void Cookie::setIngredients(const vector<string>& newIngredients){
-            ingredients.clear();
-            ingredients.insert(newIngredients.begin(),newIngredients.end());
-        }
+void Cookie::setIngredients(const vector<string>& newIngredients)
+{
+    ingredients.clear();
+    ingredients.insert(newIngredients.begin(),newIngredients.end());
+}
             
-size_t Cookie::getServings() const {return numOfServings;}
+size_t Cookie::getServings() const 
+{
+    return numOfServings;
+}
 
-set<string> Cookie::getIngredients() const {return ingredients;}
+set<string> Cookie::getIngredients() const 
+{
+    return ingredients;
+}
 
-Cookie& Cookie::getCookie() {return *this;}
+Cookie& Cookie::getCookie() 
+{
+    return *this;
+}
 
-void Cookie::printRecipe() const {
+void Cookie::printRecipe() const 
+{
     cout << "Recipe for " << getName();
     cout << "\nServings: " << numOfServings;
     printIngredients();
 }
 
-void Cookie::printCalories() const{
+void Cookie::printCalories() const
+{
     cout << getName() << "(calories: " << getCalories() << ")\n";
-    }
+}
 
-void Cookie::printIngredients() const{
-            cout << "Ingredients: ";
-
-            for(auto i : ingredients)
-                cout << i << " ";
-        }
+void Cookie::printIngredients() const
+{
+    cout << "Ingredients: ";
+    for(auto i : ingredients)
+        cout << i << " ";
+}
