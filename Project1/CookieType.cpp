5bb481441e781207f92b32405cad4238cbc5a614
Thu Nguyen <70258727+ThuNguyen12@users.noreply.github.com>
2021-05-17 23:22:13 -0700

    Update CookieType.cpp

--- Project1/CookieType.cpp
+++ Project1/CookieType.cpp
@@ -14,23 +14,28 @@
 
 using namespace std;
 
-CookieType::CookieType(const string& name, size_t calorieNum){ 
+CookieType::CookieType(const string& name, size_t calorieNum)
+{ 
     cookieName = name; 
     calories = calorieNum;
 }
     
-string CookieType::getName() const {
+string CookieType::getName() const 
+{
     return cookieName;
 }
 
-size_t CookieType::getCalories() const {
+size_t CookieType::getCalories() const 
+{
     return calories;
 }
     
-void CookieType::setName(const string& newName){
+void CookieType::setName(const string& newName)
+{
     cookieName = newName;
 }
 
-void CookieType::setCalories(size_t newCalories){
+void CookieType::setCalories(size_t newCalories)
+{
     calories = newCalories;
 }
